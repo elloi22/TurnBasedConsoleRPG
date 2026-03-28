1073c11f635e6eb1f55d9e1b451c666dfbae95a0
ramzorevelo <vncramzo@gmail.com>
2026-03-28 19:40:21 +0800

    Add drop to Enemy

--- TurnBasedRPG/Enemy.h
+++ TurnBasedRPG/Enemy.h
@@ -1,16 +1,21 @@
 #pragma once
 #include "Character.h"
 #include "ActionResult.h"
+#include <optional>
+#include "Drop.h"
+
 // Represents a single enemy unit.
 // Inherits HP from Character and adds a Toughness break gauge.
 class Enemy : public Character
 {
 public:
-    Enemy(std::string name, int maxHp, int maxToughness);
-
+    Enemy(std::string name, int maxHp, int maxToughness, std::optional<Drop> drop = std::nullopt);
     int  getToughness()    const;
     int  getMaxToughness() const;
     bool isBroken()        const;
+    bool hasDrop() const;
+    const std::optional<Drop>& getDrop() const;
+    std::optional<Drop> dropLoot();
 
     // Reduces the break gauge by amount.
     // Sets isBroken() and resets the gauge when it reaches zero.
@@ -23,9 +28,13 @@ public:
     // // Derived classes override this to implement specific attack patterns.
     virtual ActionResult performAttack();
 
+
+
+
 private:
     int  m_toughness{};
     int  m_maxToughness{};
     bool m_isBroken{ false };
+    std::optional<Drop> m_drop{};
 };
 
